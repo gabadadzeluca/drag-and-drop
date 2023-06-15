a06052250629cfe5aad9916a0c9af1659177342e
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-16 00:50:59 +0400

    initial commit; finish project except styles

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [fruitList, setFruitList] = useState<string[]>([
+    "apple",
+    "banana",
+    "peach",
+    "pineapple",
+    "kiwi",
+  ]);
+  console.log(fruitList);
+  const dragItem = useRef<any>(null);
+  const dragOverItem = useRef<any>(null);
+
+  const handleSort = () => {
+    let duplicatedArray = [...fruitList];
+
+    // remove and save the dragged item content
+    const draggedItemContent = duplicatedArray.splice(dragItem.current, 1)[0];
+
+    // switch positions
+    duplicatedArray.splice(dragOverItem.current, 0, draggedItemContent);
+
+    // update the state
+    setFruitList(duplicatedArray);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        {fruitList.map((item, index) => (
+          <div
+            className="list"
+            draggable
+            onDragEnter={() => (dragItem.current = index)}
+            onDragStart={() => (dragOverItem.current = index)}
+            onDragEnd={handleSort}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            <h3 key={index}>{item}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
