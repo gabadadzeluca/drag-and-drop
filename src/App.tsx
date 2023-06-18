d815cd522de022902199425e40990a42c6e962a0
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-18 18:40:31 +0400

    start working on a new version

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,47 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [fruitList, setFruitList] = useState<string[]>([
+  const [listOne, setListOne] = useState<string[]>([
     "apple",
     "banana",
     "peach",
     "pineapple",
     "kiwi",
   ]);
-  console.log(fruitList);
+  const [listTwo, setListTwo] = useState<string[]>([]);
+  
+  console.log("List 1:",listOne);
+  console.log("List 2:",listTwo);
+
   const dragItem = useRef<any>(null);
   const dragOverItem = useRef<any>(null);
 
   const handleSort = () => {
-    let duplicatedArray = [...fruitList];
-
-    // remove and save the dragged item content
-    const draggedItemContent = duplicatedArray.splice(dragItem.current, 1)[0];
-
-    // switch positions
-    duplicatedArray.splice(dragOverItem.current, 0, draggedItemContent);
-
-    // update the state
-    setFruitList(duplicatedArray);
+    
   };
 
   return (
     <div className="App">
-      <div>
-        {fruitList.map((item, index) => (
-          <div
-            key={index}
-            className="listItem"
-            draggable
-            onDragEnter={() => (dragItem.current = index)}
-            onDragStart={() => (dragOverItem.current = index)}
-            onDragEnd={handleSort}
-            onDragOver={(e) => e.preventDefault()}
-          >
-            <h3>{item}</h3>
-          </div>
-        ))}
+      <div className="container">
+        <div className="list1">
+          {listOne.map((item, index) => (
+            <div
+              key={index}
+              className="listItem"
+              draggable
+              onDragEnter={() => {}}
+              onDragStart={() => {}}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <h3>{item}</h3>
+            </div>
+          ))}
+        </div>
+
+        <div className="list2">
+          {}
+        </div>
       </div>
     </div>
   );
