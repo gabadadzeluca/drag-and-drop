af2d3fa00dd5452c277293497bf24606592b1fec
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-18 19:38:23 +0400

    add a general function for changing values; finish logical side of the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
 
+const handleDragEnd = (
+  arr1: string[],
+  arr2: string[],
+  id: number,
+  ref1: any,
+  ref2: any,
+  setter1: React.Dispatch<React.SetStateAction<string[]>>,
+  setter2: React.Dispatch<React.SetStateAction<string[]>>
+) => {
+  if (ref1.current === ref2.current) {
+    const duplicatedListOne = [...arr1];
+    const duplicatedListTwo = [...arr2];
+
+    const removedItem = duplicatedListOne.splice(id, 1)[0];
+
+    duplicatedListTwo.push(removedItem);
+
+    setter1(duplicatedListOne);
+    setter2(duplicatedListTwo);
+  }
+};
+
 function App() {
   const [listOne, setListOne] = useState<string[]>([
     "apple",
@@ -10,44 +32,38 @@ function App() {
     "kiwi",
   ]);
   const [listTwo, setListTwo] = useState<string[]>([]);
-  
+
+  const listOneRef = useRef<HTMLDivElement>(null);
   const listTwoRef = useRef<HTMLDivElement>(null);
-  const dragItem = useRef<any>(null);
-
-
-  console.log("List 1:",listOne);
-  console.log("List 2:",listTwo);
-
-  const handleDragEnd = (id:number) => {
-    if(dragItem.current === listTwoRef.current){
-      
-      const duplicatedListOne = [...listOne];
-      const duplicatedListTwo = [...listTwo];
-
-      const removedItem = duplicatedListOne.splice(id, 1)[0];
-
-      duplicatedListTwo.push(removedItem);
-      
-      console.log("setting...");
-      setListOne(duplicatedListOne);
-      setListTwo(duplicatedListTwo);
-    }
-  }
-
-  const handleDragEnter = (e:React.DragEvent<HTMLDivElement>) => {
-    dragItem.current = e.currentTarget;
-  }
+  const dragItemOne = useRef<any>(null);
+  const dragItemTwo = useRef<any>(null);
 
   return (
     <div className="App">
       <div className="container">
-        <div className="list1">
+        <div
+          className="list1"
+          ref={listOneRef}
+          onDragEnter={(e) => {
+            dragItemTwo.current = e.currentTarget;
+          }}
+        >
           {listOne.map((item, index) => (
             <div
               key={index}
               className="listItem"
               draggable
-              onDragEnd={()=>handleDragEnd(index)}
+              onDragEnd={() =>
+                handleDragEnd(
+                  listOne,
+                  listTwo,
+                  index,
+                  dragItemOne,
+                  listTwoRef,
+                  setListOne,
+                  setListTwo
+                )
+              }
             >
               <h3>{item}</h3>
             </div>
@@ -57,13 +73,26 @@ function App() {
         <div
           className="list2"
           ref={listTwoRef}
-          draggable
-          onDragEnter={handleDragEnter}
+          onDragEnter={(e: React.DragEvent<HTMLDivElement>) => {
+            dragItemOne.current = e.currentTarget;
+          }}
         >
-          {listTwo.map((item, index)=>(
+          {listTwo.map((item, index) => (
             <div
+              draggable
               key={index}
               className="listItem"
+              onDragEnd={() =>
+                handleDragEnd(
+                  listTwo,
+                  listOne,
+                  index,
+                  dragItemTwo,
+                  listOneRef,
+                  setListTwo,
+                  setListOne
+                )
+              }
             >
               {item}
             </div>
